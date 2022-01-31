9ecdfe043e2a01bb9daf4f2ef6698f4009b576ef
Rodrigo Tores <rtorres@munidigital.com>
2022-01-31 20:17:03 -0300

    Set ListFoodRouter

---
+++ challenge-food/challenge-food/UseCases/ListFood/ListFoodRouter.swift
@@ -0,0 +1,30 @@
+//
+//  ListFoodRouter.swift
+//  challenge-food
+//
+//  Created by Rodrigo Torres on 31/01/2022.
+//
+
+import Foundation
+import UIKit
+
+class ListFoodRouter {
+    
+    var viewController: UIViewController {
+        return createViewController()
+    }
+    
+    private var sourceView: UIViewController?
+    
+    func createViewController() -> UIViewController {
+        let view = ListFoodVC(nibName: "ListFoodVC", bundle: nil)
+        view.title = "Lista de Comidas"
+        return view
+    }
+    
+    func setSourceView(_ sourceView: UIViewController?) {
+        guard let view = sourceView else {fatalError("Error")}
+        self.sourceView = view
+    }
+    
+}
