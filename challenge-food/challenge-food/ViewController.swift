80bb9fa23c6b2e91eb569140d17fd489d10d5abe
Rodrigo Tores <rtorres@munidigital.com>
2022-01-31 19:39:20 -0300

    Project setup

---
+++ challenge-food/challenge-food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge-food
+//
+//  Created by Rodrigo Torres on 31/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
