d22f2f47a4ab49e801664d69fbdf8f01d17c0fb0
Rodrigo Tores <rtorres@munidigital.com>
2022-02-01 23:45:27 -0300

    Add Request Alamofire and Constants for API

--- challenge-food/challenge-food/Network/APIManager.swift
+++ challenge-food/challenge-food/Network/APIManager.swift
@@ -11,6 +11,22 @@ import Alamofire
 
 class APIManager {
     
-    func getListFood() -> Observable<[]>
+    func getListFood(search: String) -> Observable<[Foods]> {
+        let url = Constants.URL.main+Constants.Endpoint.search+search
+        return Observable.create { observer in
+            let requestReference = AF.request(url).responseDecodable(of: Foods.self) { response in
+                switch response.result {
+                case .success(let data):
+                    observer.onNext(data.listOfFoods)
+                case .failure(let error):
+                    observer.onError(error.underlyingError!)
+                }
+            }
+            return Disposables.create {
+                requestReference.cancel()
+            }
+            
+        }
+    }
     
 }
