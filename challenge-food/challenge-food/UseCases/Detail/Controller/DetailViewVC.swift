d694fd1832c25d82b43a9758bfee79767a13edc1
Rodrigo Tores <rtorres@munidigital.com>
2022-02-05 23:27:18 -0300

    Auto stash before revert of "Add cell for ingredientsTableView and set logic "

--- challenge-food/challenge-food/UseCases/Detail/Controller/DetailViewVC.swift
+++ challenge-food/challenge-food/UseCases/Detail/Controller/DetailViewVC.swift
@@ -18,26 +18,79 @@ class DetailViewVC: UIViewController {
     
     var idFood: String?
     private var detailFood = [Food]()
+    private var ingredientes = [String]()
     private var disposeBag = DisposeBag()
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         getDataDetail()
+        setupTableView()
     }
     
     private func getDataDetail() {
         guard let id = idFood else { return }
-        
         viewModel.getDetailData(idMeal: id)
             .subscribe(on: MainScheduler.instance)
             .observe(on: MainScheduler.instance)
             .subscribe { foods in
                 self.detailFood = foods
-                print(self.detailFood)
+                self.ingredientes = self.getIngredients(food: foods[0])
+                self.ingredientsTableView.reloadData()
             } onError: { error in
                 print(error.localizedDescription)
             }.disposed(by: disposeBag)
     }
+    
+    private func showDetail(data: Food) {
+        titleLabel.text = data.strMeal
+        descriptionLabel.text = data.strInstructions
+    }
+    
+    private func getIngredients(food: Food) -> [String] {
+        let array = Food.getIngredintes(data: food)
+        return array
+    }
 
 }
+
+extension DetailViewVC: UITableViewDelegate, UITableViewDataSource {
+    
+    private func setupTableView() {
+        ingredientsTableView.register(IngredientsCell.nib(), forCellReuseIdentifier: IngredientsCell.identifier)
+        ingredientsTableView.dataSource = self
+        ingredientsTableView.delegate = self
+        ingredientsTableView.rowHeight = UITableView.automaticDimension
+        ingredientsTableView.estimatedRowHeight = UITableView.automaticDimension
+        ingredientsTableView.bounces = false
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Ingredientes"
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = .black
+        return label
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ingredientes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ingredientsTableView.dequeueReusableCell(withIdentifier: IngredientsCell.identifier, for: indexPath) as! IngredientsCell
+        cell.setupCell(ingredientsText: ingredientes[indexPath.row])
+        return cell
+    }
+    
+}
+
+
--- challenge-food/challenge-food/UseCases/ListFood/Model/Food.swift
+++ challenge-food/challenge-food/UseCases/ListFood/Model/Food.swift
@@ -11,15 +11,88 @@ struct Food: Codable {
     
     let idMeal: String
     let strMeal: String
+    let strInstructions: String
     let strCategory: String
     let strMealThumb: String
+    
+    let strIngredient1: String
+    let strIngredient2: String
+    let strIngredient3: String
+    let strIngredient4: String
+    let strIngredient5: String
+    let strIngredient6: String
+    let strIngredient7: String
+    let strIngredient8: String
+    let strIngredient9: String
+    let strIngredient10: String
+    let strIngredient11: String
+    let strIngredient12: String
+    let strIngredient13: String
+    let strIngredient14: String
+    let strIngredient15: String
+    let strIngredient16: String
+    let strIngredient17: String
+    let strIngredient18: String
+    let strIngredient19: String
+    let strIngredient20: String
+
+    
     
     enum CodingKeys: String, CodingKey {
         case idMeal
         case strMeal
+        case strInstructions
         case strCategory
         case strMealThumb
+        
+        case strIngredient1
+        case strIngredient2
+        case strIngredient3
+        case strIngredient4
+        case strIngredient5
+        case strIngredient6
+        case strIngredient7
+        case strIngredient8
+        case strIngredient9
+        case strIngredient10
+        case strIngredient11
+        case strIngredient12
+        case strIngredient13
+        case strIngredient14
+        case strIngredient15
+        case strIngredient16
+        case strIngredient17
+        case strIngredient18
+        case strIngredient19
+        case strIngredient20
     }
     
     
+    static func getIngredintes(data: Food) -> [String] {
+        let array = [
+            data.strIngredient1,
+            data.strIngredient2,
+            data.strIngredient3,
+            data.strIngredient4,
+            data.strIngredient5,
+            data.strIngredient6,
+            data.strIngredient7,
+            data.strIngredient8,
+            data.strIngredient9,
+            data.strIngredient10,
+            data.strIngredient11,
+            data.strIngredient12,
+            data.strIngredient13,
+            data.strIngredient14,
+            data.strIngredient15,
+            data.strIngredient16,
+            data.strIngredient17,
+            data.strIngredient18,
+            data.strIngredient19,
+            data.strIngredient20,
+        ]
+        
+        let mapArray = array.filter {$0 != ""}
+        return mapArray
+    }
 }
